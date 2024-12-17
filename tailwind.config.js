9459cfbc1ddaaec9ac848b0a1ba9e13928402d68
Igor <silent659@gmail.com>
2024-12-17 06:00:17 +0200

    start

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const options = require("./config"); //options from config.js
+
+const allPlugins = {
+  typography: require("@tailwindcss/typography"),
+  forms: require("@tailwindcss/forms"),
+  containerQueries: require("@tailwindcss/container-queries"),
+  UI: require("daisyui"),
+};
+
+const plugins = Object.keys(allPlugins)
+  .filter((k) => options.plugins[k])
+  .map((k) => {
+    if (k in options.plugins && options.plugins[k]) {
+      return allPlugins[k];
+    }
+  });
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: plugins,
+};
